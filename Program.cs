38fa3b0a84023e910422937958fd92487e82e8cd
inkoz <inkoz@DESKTOP-JS5JFS1>
2022-07-15 18:25:46 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+
+        static void FillArrayRandomNumbers(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+                }
+            }
+        }
+
+        static void PrintArray(double[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write("[ ");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.Write("]");
+                Console.WriteLine("");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите количество строк");
+            int linesVol = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите количество столбцов");
+            int columnsVol = Convert.ToInt32(Console.ReadLine());
+            double[,] numbers = new double[linesVol, columnsVol];
+            FillArrayRandomNumbers(numbers);
+            PrintArray(numbers);
+
+        }
+    }
+}
